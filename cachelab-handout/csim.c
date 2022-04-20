91fff3b79059fa3799fee7f856ca6c95a0b9c8fa
Wangxiaohuo0903 <slow_walker@sjtu.edu.cn>
2022-04-20 16:09:01 +0800

    cache lab finished

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,197 @@
 #include "cachelab.h"
-
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+//csim.c by huo
+typedef struct
 {
-    printSummary(0, 0, 0);
-    return 0;
+    int valid;
+    int tag;
+    int wait_time;
+} cache_line,*cache_set,**cache;
+    int s_num,E_num,b_num;
+    int S_num;
+    int v_flag=0;
+    char *file_txt;
+    cache test_cache=NULL;
+    int hit=0,miss=0,evi=0;
+    int h_flag=0,m_flag=0,e_flag=0;
+void init_cache()
+{
+test_cache=(cache)malloc(sizeof(cache_set) * S_num);
+	for(int i = 0; i < S_num; ++i)
+	{
+		test_cache[i] = (cache_set)malloc(sizeof(cache_line) * E_num);
+		for(int j = 0; j < E_num; ++j)
+		{
+			test_cache[i][j].valid = 0;
+			test_cache[i][j].tag = -1;
+			test_cache[i][j].wait_time = -1;
+		}
+	}
+    return;
+}
+void update(unsigned int address)
+{
+int tag=address>>(b_num+s_num);
+int set= (address <<(64-b_num-s_num))>>(64-s_num);
+//printf("\nAAA%d,%d\n",tag,set);
+int find_tag=0;
+//try to find,if tag=-1,insert one
+for(int i=0;i<E_num;i++)
+{
+    if(test_cache[set][i].tag==-1) //empty
+    {
+        test_cache[set][i].tag=tag;
+        test_cache[set][i].wait_time=-1;
+        find_tag=1;
+        miss++;
+        if(v_flag)
+        printf("miss ");
+        //m_flag=1;
+        break;
+    }
+    else if(test_cache[set][i].tag==tag)
+    {
+        test_cache[set][i].wait_time=-1;
+        find_tag=1;
+        hit++;
+        if(v_flag)
+        printf("hit ");
+        //h_flag=1;
+        break;
+    }
+
+}
+//010000
+//100000
+//110000
+//all wait_time add 1
+
+    for(int j=0;j<E_num;j++)
+    {
+        if(test_cache[set][j].tag!=-1)
+        test_cache[set][j].wait_time++;
+    }
+
+//if not find,exchange one
+if(!find_tag){
+int tmp=test_cache[set][0].wait_time;
+int t_mun=0;
+for(int i=1;i<E_num;i++)
+{
+if(tmp<test_cache[set][i].wait_time)
+{tmp=test_cache[set][i].wait_time;
+t_mun=i;
+}
+}
+//printf("EVICTION from %x to %x",test_cache[set][t_mun].tag,tag);
+test_cache[set][t_mun].tag=tag;
+test_cache[set][t_mun].wait_time=0;
+evi++;
+miss++;
+// m_flag=1;
+// e_flag=1;
+if(v_flag)
+printf("miss eviction ");
+}
+
+return;
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	char ch;
+//read seq
+
+	while((ch = getopt(argc, argv, "hvs:E:b:t:")) != EOF)
+	{
+		switch(ch)
+		{
+			case 'h':
+printf("\
+Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n\
+Options:\n\
+  -h         Print this help message.\n\
+  -v         Optional verbose flag.\n\
+  -s <num>   Number of set index bits.\n\
+  -E <num>   Number of lines per set.\n\
+  -b <num>   Number of block offset bits.\n\
+  -t <file>  Trace file.\n\
+\n\
+Examples:\n\
+  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n\
+  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+				break;
+
+            case 's':
+                s_num=atoi(optarg);
+                S_num=1<<s_num;
+               // printf("s_num:%d\n",s_num)
+                break;
+            case 'E':
+                E_num=atoi(optarg);
+                break;
+            case 'b':
+                b_num=atoi(optarg);
+                break;
+            case 't':
+                file_txt=optarg;
+                //strcpy(file_txt,optarg);
+                break;
+            case 'v':
+                v_flag=1;
+                break;
+			default:
+				return 1;
+		}
+	} 
+
+//init cache
+init_cache(test_cache);
+
+//read trace
+    FILE* fp = fopen(file_txt, "r"); // 读取文件名
+	if(fp == NULL)
+	{
+		printf("open error");
+		exit(-1);
+	}
+	
+	char operation;         // 命令开头的 I L M S
+	unsigned int address;   // 地址参数
+	int size;               // 大小
+	while(fscanf(fp, " %c %x,%d\n", &operation, &address, &size) > 0)
+	{
+        if(v_flag)
+        printf(" %c %x,%d\t", operation, address, size);
+        switch (operation)
+        {
+        case 'I':
+            /* code */
+            break;
+        case 'L':
+            update(address);
+            break;
+        case 'M':
+            update(address);
+        case 'S':
+            update(address);
+            break;            
+        default:
+            break;
+        }
+        if(v_flag)
+        printf("\n");
+        
+    }
+	fclose(fp);
+	for(int i = 0; i < S_num; ++i)
+		free(test_cache[i]);
+	free(test_cache); 
+    printSummary(hit, miss, evi);
+	return 0;
 }
--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -6,7 +6,7 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
@@ -19,31 +19,148 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     searches for that string to identify the transpose function to
  *     be graded. 
  */
+//(s=5, E=1, b=5)
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j;
+    if (M == 32)
+    {
+        int k;
+        for (i = 0; i < N; i += 8)
+        {
+            for (j = 0; j < M; j += 8)
+            {
+                for (k = i; k < 8 + i; k++)
+                {
+                        int v1, v2, v3, v4, v5, v6, v7, v8;
+                        v1 = A[k][j];
+                        v2 = A[k][j + 1];
+                        v3 = A[k][j + 2];
+                        v4 = A[k][j + 3];
+                        v5 = A[k][j + 4];
+                        v6 = A[k][j + 5];
+                        v7 = A[k][j + 6];
+                        v8 = A[k][j + 7];
+                        B[j][k] = v1;
+                        B[j + 1][k] = v2;
+                        B[j + 2][k] = v3;
+                        B[j + 3][k] = v4;
+                        B[j + 4][k] = v5;
+                        B[j + 5][k] = v6;
+                        B[j + 6][k] = v7;
+                        B[j + 7][k] = v8;
+                    
+                }
+            }
+        }
+    }
+    else if (M == 64)
+    {
+        
+        for(i=0;i<64;i+=8)
+        {
+            for(j=0;j<64;j+=8)
+            {
+                //a b insert and reverse
+                int v0,v1,v2,v3,v4,v5,v6,v7;
+                for(int k=0;k<4;k++)
+                {  
+                    v0=A[k+i][j];v1=A[k+i][j+1];v2=A[k+i][j+2];v3=A[k+i][j+3];
+                    v4=A[k+i][j+4];v5=A[k+i][j+5];v6=A[k+i][j+6];v7=A[k+i][j+7];
+                    B[j][i+k]=v0;B[j+1][i+k]=v1;B[j+2][i+k]=v2;B[j+3][i+k]=v3;
+                    B[j][i+k+4]=v4;B[j+1][i+4+k]=v5;B[j+2][i+4+k]=v6;B[j+3][i+4+k]=v7;
+
+                }
+                for(int k=0;k<4;k++)
+                {
+                    v4=B[j+k][i+4];v5=B[k+j][i+5];v6=B[k+j][i+6];v7=B[k+j][i+7];
+                    v0=A[i+4][j+k];v1=A[i+5][j+k];v2=A[i+6][j+k];v3=A[i+7][j+k];
+                    B[k+j][i+4]=v0;B[k+j][i+5]=v1;B[k+j][i+6]=v2;B[k+j][i+7]=v3;
+                    B[k+j+4][i]=v4;B[k+j+4][i+1]=v5;B[k+j+4][i+2]=v6;B[k+j+4][i+3]=v7;
+                    
+                }
+                for(int k=0;k<4;k++)
+                {  
+                    
+                    v4=A[k+i+4][j+4];v5=A[k+i+4][j+5];v6=A[k+i+4][j+6];v7=A[k+i+4][j+7];
+                    B[j+4][i+k+4]=v4;B[j+5][i+4+k]=v5;B[j+6][i+4+k]=v6;B[7+j][i+4+k]=v7;
+
+                }
+               
+   
+
+            }
+        }
+    }
+        else 
+    {
+        //int b = 8; //M=61,N=67，b=8，
+        //int tmp;
+        //int en = b * (N / b), em = b * (M / b);
+        int k;
+        for (i = 0; i <= 64; i += 8)
+        {
+            for (j = 0; j <= 56; j += 8)
+            {
+                for (k = i; k < 8 + i && k < N; k++)
+                {
+                    //if(1==0)
+                    if(i<64&&j<56)
+                    {
+
+                        int v1, v2, v3, v4, v5, v6, v7, v8;
+                        v1 = A[k][j];
+                        v2 = A[k][j + 1];
+                        v3 = A[k][j + 2];
+                        v4 = A[k][j + 3];
+                        v5 = A[k][j + 4];
+                        v6 = A[k][j + 5];
+                        v7 = A[k][j + 6];
+                        v8 = A[k][j + 7];
+                        B[j][k] = v1;
+                        B[j + 1][k] = v2;
+                        B[j + 2][k] = v3;
+                        B[j + 3][k] = v4;
+                        B[j + 4][k] = v5;
+                        B[j + 5][k] = v6;
+                        B[j + 6][k] = v7;
+                        B[j + 7][k] = v8;
+                    }
+                    else
+                    for (int jj = j; jj < 8 + j && jj < M; jj++)
+                    {
+                   
+                        B[jj][k] = A[k][jj];
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
- */ 
+ */
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
+//(s=5, E=1, b=5)
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -56,11 +173,10 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(trans, trans_desc);
 }
 
 /* 
@@ -72,13 +188,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
